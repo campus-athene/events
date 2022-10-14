331ce723d76da632d219a2c746139c6d20a522e0
Oliver Mayer <git@oliverrm.de>
2022-10-14 19:36:06 +0200

    Redesign newsletter box

--- pages/preview.tsx
+++ pages/preview.tsx
@@ -275,18 +275,17 @@ const Home = (props: Data) => {
           </EventGroup>
         </>
       )}
-      <div className="bg-violet mt-8 py-8 px-10 text-white">
+      <div className="bg-violet mt-8 mx-10 py-10 px-10 rounded-xl text-white">
         <div className="mb-2 text-lg">Verpasse keine weiteren Events mehr!</div>
-        <div className="mb-4">
-          Erhalte einmal pro Woche einen auf Dich persönlich zugeschnittenen
-          Newsletter.
+        <div className="mb-8">
+          Erhalte wöchentlich unseren Newsletter mit den kommenden Highlights.
         </div>
         <input
-          className="bg-slate-600 rounded-l-full pl-3 pr-2"
+          className="bg-slate-600 rounded-l-full py-2 pl-6 pr-2 placeholder-slate-300"
           placeholder="E-Mail-Adresse"
           type="text"
         />
-        <button className="bg-amber-500 pl-4 pr-5 rounded-r-full">
+        <button className="bg-amber-500 py-2 pl-8 pr-9 rounded-r-full text-white font-medium">
           Abbonieren
         </button>
       </div>
